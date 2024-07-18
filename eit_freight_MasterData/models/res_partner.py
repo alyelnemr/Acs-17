bb70078cb090029bf0c304b041b9c7414a5d78cd
ridhin <ridhinm@atconline.biz>
2024-07-18 14:53:11 +0530

    updates

--- eit_freight_MasterData/models/res_partner.py
+++ eit_freight_MasterData/models/res_partner.py
@@ -7,4 +7,4 @@ class InheritResPartner(models.Model):
     partner_type_id = fields.Many2many('partner.type', string="Partner Type", required=True)
     created_by = fields.Many2one('res.users', default=lambda self: self.env.user.id, string="Created by", readonly=True)
     excecuters = fields.Many2many('res.users', string="Executors")
-    show_partner = fields.Boolean()
+    # show_partner = fields.Boolean()
