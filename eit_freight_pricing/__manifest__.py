7cdb48b75e6a501e008cc296adb7e94169d728a1
ridhin <ridhinm@atconline.biz>
2024-07-24 15:45:17 +0530

    updates

--- eit_freight_pricing/__manifest__.py
+++ eit_freight_pricing/__manifest__.py
@@ -2,11 +2,9 @@
 {
     'name': "Pricing",
 
-    'summary': "The pricing module is for publishing shipping rates on the website with integration to get a Quote or Make a booking online",
+    'summary': """Freight Forwarding Pricing""",
 
-    'description': """
-    The pricing module is for publishing shipping rates on the website with integration to get a Quote or Make a booking online, as we can request prices and RFQ to compare the rates
-    """,
+    'description': """The pricing module is for publishing shipping rates on the website with integration to get a Quote or Make a booking online""",
 
     'author': "EIT-Hub (Expert Information Technology) ",
     'website': "www.eit-hub.com",
@@ -14,9 +12,10 @@
     'category': 'Services/Freight',
     'version': '17.0',
     'depends': ['base', 'purchase', 'eit_freight_MasterData', 'account', 'base_setup', 'product', 'base_setup',
-                'mail', 'website_sale','hr_expense'],
+                'mail', 'website_sale', 'hr_expense'],
 
     'data': [
+        'security/pricing_secuirity.xml',
         'security/ir.model.access.csv',
         'security/stage_pricing_data.xml',
         'views/product_template_view.xml',
@@ -26,11 +25,11 @@
         'views/sale_order_view.xml',
         'wizard/requst_price_vendor_view.xml',
 
-
     ],
 
     'installable': True,
     'application': True,
     'auto_install': True,
+    'post_init_hook': 'post_init_hook',
     'license': 'LGPL-3',
 }
