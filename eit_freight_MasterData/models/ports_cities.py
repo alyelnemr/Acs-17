cd4c653d6a3ce9968b2bf094e64fbf9b5f70f1e8
a.abdelhamid <a.abdelhamid@fayendra.com>
2024-07-25 15:40:46 +0300

    [fix] set country group in port cities

--- eit_freight_MasterData/models/ports_cities.py
+++ eit_freight_MasterData/models/ports_cities.py
@@ -11,9 +11,8 @@ class PortCitiesTemplate(models.Model):
     name = fields.Char(string="Name")
     code = fields.Char(string="Code")
     country_id = fields.Many2one('res.country', string="Country", required=True)
-    country_group_ids = fields.Many2many('res.country.group', string='Country Group',
-                                         related="country_id.country_group_ids")
-    display_name = fields.Char(string="Display Name", compute="compute_dispaly_name")
+    country_group_ids = fields.Many2many('res.country.group', string='Country Group')
+    display_name = fields.Char(string="Display Name", compute="compute_display_name")
     type = fields.Selection([('air', 'Air'), ('sea', 'Sea'), ('island', 'Inland')], 'Is')
     status = fields.Selection([('active', 'Active'), ('inactive', 'Inactive')], 'Active', readonly=True)
     active = fields.Boolean(string='Status', default=True)
@@ -21,9 +20,10 @@ class PortCitiesTemplate(models.Model):
     country_group_id_1 = fields.Many2one('res.country.group', string='Country Group',
                                          compute="compute_country_group_id", store=True)
 
-    @api.model
-    def _default_country_group_ids(self):
-        return self.env['res.country.group'].search([]).ids
+    @api.onchange('country_id')
+    def _onchange_country_id(self):
+        if self.country_id:
+            self.country_group_ids = self.country_id.country_group_ids
 
     @api.depends('country_id')
     def compute_country_group_id(self):
@@ -34,7 +34,7 @@ class PortCitiesTemplate(models.Model):
                 rec.country_group_id_1 = None
 
     @api.depends('country_id', 'code')
-    def compute_dispaly_name(self):
+    def compute_display_name(self):
         for rec in self:
             str = ""
             if rec.name:
@@ -50,23 +50,32 @@ class PortCitiesTemplate(models.Model):
                 rec.toggle_active()
 
     def create(self, vals):
+        # Create the record first
         record = super(PortCitiesTemplate, self).create(vals)
-        if 'country_group_ids' in vals and record.country_id:
-            current_groups = record.country_id.country_group_ids.ids
-            new_groups = vals['country_group_ids'][0][2]
-            if set(current_groups) != set(new_groups):
-                record.country_id.write({'country_group_ids': [(6, 0, new_groups)]})
+
+        # Update country_group_ids directly if both country_group_ids and country_id are present in vals
+        if 'country_group_ids' in vals and 'country_id' in vals:
+            country = self.env['res.country'].browse(vals['country_id'])
+            # Flatten the list of operations to get the new groups
+            new_groups = []
+            for operation in vals['country_group_ids']:
+                if operation[0] == 4:
+                    new_groups.append(operation[1])
+                elif operation[0] == 6:
+                    new_groups = operation[2]
+                    break
+            if set(new_groups) != set(country.country_group_ids.ids):
+                country.sudo().write({'country_group_ids': [(6, 0, new_groups)]})
+
         return record
 
     def write(self, vals):
         result = super(PortCitiesTemplate, self).write(vals)
         if 'country_group_ids' in vals:
+            new_groups = vals['country_group_ids'][0][2]
             for record in self:
-                if record.country_id:
-                    current_groups = record.country_id.country_group_ids.ids
-                    new_groups = vals['country_group_ids'][0][2]
-                    if set(current_groups) != set(new_groups):
-                        record.country_id.write({'country_group_ids': [(6, 0, new_groups)]})
+                if record.country_id and set(record.country_id.country_group_ids.ids) != set(new_groups):
+                    record.country_id.update({'country_group_ids': [(6, 0, new_groups)]})
         return result
 
 
