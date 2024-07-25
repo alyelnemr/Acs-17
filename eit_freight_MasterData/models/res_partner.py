216c10f403e9d91eec87262ebbbef8e541973c90
a.abdelhamid <a.abdelhamid@fayendra.com>
2024-07-25 15:54:03 +0300

    [fix] partner type for parent

--- eit_freight_MasterData/models/res_partner.py
+++ eit_freight_MasterData/models/res_partner.py
@@ -34,24 +34,8 @@ class InheritResPartner(models.Model):
         return super(InheritResPartner, self).create(vals_list)
 
     def write(self, vals):
-        result = super(InheritResPartner, self).write(vals)
         if 'parent_id' in vals:
             parent = self.search([('id', '=', vals['parent_id'])])
-            if parent and parent.company_type == 'company' and parent.partner_type_id:
+            if parent and parent.company_type == 'company' and not parent.partner_type_id:
                 vals['partner_type_id'] = [(6, 0, parent.partner_type_id.ids)]
-
-        if 'country_group_ids' in vals:
-            new_groups = set()
-            for operation in vals['country_group_ids']:
-                if operation[0] == 4:  # Link operation
-                    new_groups.add(operation[1])
-                elif operation[0] == 3:  # Unlink operation
-                    if operation[1] in new_groups:
-                        new_groups.remove(operation[1])
-
-            for record in self:
-                if record.country_id:
-                    current_groups = set(record.country_id.country_group_ids.ids)
-                    if current_groups != new_groups:
-                        record.country_id.sudo().write({'country_group_ids': [(6, 0, list(new_groups))]})
-        return result
+        return super(InheritResPartner, self).write(vals)
