8ad730b521d56054bc6e82a90b50e3dce5b0103d
a.abdelhamid <a.abdelhamid@fayendra.com>
2024-09-18 17:55:21 +0300

    [add] some comments from task 12-Sep

--- eit_freight_pricing/models/res_partner.py
+++ eit_freight_pricing/models/res_partner.py
@@ -1,31 +1,43 @@
 from odoo import models, fields, api
 
-
 class InheritResPartner(models.Model):
     _inherit = "res.partner"
 
     price_count = fields.Integer(string="Price Count", compute='get_price_count')
 
     def action_view_pricing(self):
-        self.ensure_one()
-        return {
-            'type': 'ir.actions.act_window',
-            'binding_type': 'object',
-            'domain': [('partner_id', '=', self.id), ('detailed_type', '=', 'pricing')],
-            'multi': False,
-            'name': 'Pricing',
-            'target': 'current',
-            'res_model': 'product.template',
-            'view_mode': 'form',
-            'view_id': self.env.ref('eit_freight_pricing.product_template_form_view_pricing2').id,
-            'context': {
-            'default_partner_id': self.id,
-            'search_default_partner_id': self.id,
-            'default_detailed_type': 'pricing'
-        },
+        product_ids = self.env['product.template'].search([('partner_id', '=', self.id), ('detailed_type', '=', 'pricing')])
 
-        }
-    
+        if len(product_ids) == 1:
+            return {
+                'type': 'ir.actions.act_window',
+                'name': 'Pricing',
+                'view_mode': 'form',
+                'res_model': 'product.template',
+                'res_id': product_ids.id,  # Open the form view with the single record
+                'view_id': self.env.ref('eit_freight_pricing.product_template_form_view_pricing2').id,
+                'target': 'current',
+                'context': {
+                    'default_partner_id': self.id,
+                    'default_detailed_type': 'pricing',
+                },
+            }
+        else:
+            # If more than one record, open the tree (list) view
+            return {
+                'type': 'ir.actions.act_window',
+                'name': 'Pricing',
+                'view_mode': 'tree,form',  # Show list view, and form when clicked
+                'res_model': 'product.template',
+                'domain': [('partner_id', '=', self.id), ('detailed_type', '=', 'pricing')],
+                'view_id': self.env.ref('eit_freight_pricing.product_template_tree_view_pricing2').id,
+                'context': {
+                    'default_partner_id': self.id,
+                    'default_detailed_type': 'pricing',
+                },
+                'target': 'current',
+            }
+
     def get_price_count(self):
         for rec in self:
             count = self.env['product.template'].search_count([('partner_id', '=', self.id), ('detailed_type', '=', 'pricing')])
